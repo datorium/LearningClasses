3d9162d8d9d3831e7ad985b112a7a07ee2d68247
Datorium <hello@datorium.lv>
2020-10-29 17:55:03 +0200

    version 17:54

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace LearningClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Greetings from Datorium!");
+            Human person = new Human();
+            person.Name = "Jenifer";
+            person.Surname = "Lopez";
+            person.Age = 16;   
+            Console.WriteLine("Name: " + person.Name + ", Surname: " + person.Surname + ", Age: " + person.Age);
+            person.Talk(); //Jenifer is talking...
+
+            Human anotherPerson = new Human();
+            anotherPerson.Name = "John";
+            anotherPerson.Surname = "Nash";
+            anotherPerson.Age = 35;
+            Console.WriteLine("Name: " + anotherPerson.Name + ", Surname: " + anotherPerson.Surname + ", Age: " + anotherPerson.Age);
+            anotherPerson.Talk(); //John is talking...
+        }
+    }
+
+    class Human
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public void Talk()
+        {
+            Console.WriteLine("I am talking...");
         }
     }
 }
